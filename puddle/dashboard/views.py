0c9d2947d3652769b259c317545f9e73ec22415e
KazMaune <kikainvr@gmail.com>
2023-06-29 13:58:18 -0500

    Added a delete button and an edit button, delete functionality works'

--- puddle/dashboard/views.py
+++ puddle/dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from item.models import Item
 from django.contrib.auth.decorators import login_required
 
@@ -9,3 +9,4 @@ def index(request):
     return render (request, 'dashboard/index.html', {
         'items': items,
     })
+    
